ea37c6dd34d06780d4996d98f62351846faeb903
cala9649 <carlos.lawrence@colorado.edu>
2018-02-15 16:43:05 -0700

    Starting scanning

--- src/hackrf/SDR_Testing/lesson1_fmradio.py
+++ src/hackrf/SDR_Testing/lesson1_fmradio.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Lesson1 Fmradio
-# Generated: Wed Jan 31 15:30:40 2018
+# Generated: Thu Feb 15 16:12:28 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -17,6 +17,7 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
+from PyQt4.QtCore import QObject, pyqtSlot
 from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
@@ -32,7 +33,6 @@ import osmosdr
 import sip
 import sys
 import time
-from gnuradio import qtgui
 
 
 class lesson1_fmradio(gr.top_block, Qt.QWidget):
@@ -41,7 +41,6 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
         gr.top_block.__init__(self, "Lesson1 Fmradio")
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Lesson1 Fmradio")
-        qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
         except:
@@ -67,13 +66,28 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 10e6
         self.channel_width = channel_width = 200e3
         self.channel_freq = channel_freq = 97.3e6
-        self.center_freq = center_freq = 97.3e6
-        self.audio_gain = audio_gain = 50
+        self.center_freq = center_freq = 97.9e6
+        self.audio_gain = audio_gain = 1
 
         ##################################################
         # Blocks
         ##################################################
-        self._audio_gain_range = Range(0, 100, 1, 50, 200)
+        self._samp_rate_options = (4e6, 10e6, 20e6, )
+        self._samp_rate_labels = ("4M", "10M", "20M", )
+        self._samp_rate_tool_bar = Qt.QToolBar(self)
+        self._samp_rate_tool_bar.addWidget(Qt.QLabel("samp_rate"+": "))
+        self._samp_rate_combo_box = Qt.QComboBox()
+        self._samp_rate_tool_bar.addWidget(self._samp_rate_combo_box)
+        for label in self._samp_rate_labels: self._samp_rate_combo_box.addItem(label)
+        self._samp_rate_callback = lambda i: Qt.QMetaObject.invokeMethod(self._samp_rate_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._samp_rate_options.index(i)))
+        self._samp_rate_callback(self.samp_rate)
+        self._samp_rate_combo_box.currentIndexChanged.connect(
+        	lambda i: self.set_samp_rate(self._samp_rate_options[i]))
+        self.top_layout.addWidget(self._samp_rate_tool_bar)
+        self._channel_freq_range = Range(88.1e6, 108.9e6, channel_width, 97.3e6, 200)
+        self._channel_freq_win = RangeWidget(self._channel_freq_range, self.set_channel_freq, "channel_freq", "counter_slider", float)
+        self.top_layout.addWidget(self._channel_freq_win)
+        self._audio_gain_range = Range(0, 10, 1, 1, 200)
         self._audio_gain_win = RangeWidget(self._audio_gain_range, self.set_audio_gain, "audio_gain", "counter_slider", float)
         self.top_layout.addWidget(self._audio_gain_win)
         self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
@@ -85,7 +99,7 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0 = qtgui.sink_c(
         	1024, #fftsize
         	firdes.WIN_HAMMING, #wintype
-        	0, #fc
+        	center_freq, #fc
         	2e6, #bw
         	"", #name
         	True, #plotfreq
@@ -96,29 +110,29 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0.set_update_time(1.0/10)
         self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-
+        
         self.qtgui_sink_x_0.enable_rf_freq(True)
-
-
-
-        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
+        
+        
+          
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
         self.osmosdr_source_0.set_sample_rate(samp_rate)
         self.osmosdr_source_0.set_center_freq(center_freq, 0)
         self.osmosdr_source_0.set_freq_corr(0, 0)
         self.osmosdr_source_0.set_dc_offset_mode(0, 0)
         self.osmosdr_source_0.set_iq_balance_mode(0, 0)
         self.osmosdr_source_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0.set_gain(10, 0)
+        self.osmosdr_source_0.set_gain(0, 0)
         self.osmosdr_source_0.set_if_gain(20, 0)
         self.osmosdr_source_0.set_bb_gain(20, 0)
-        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_antenna("", 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
-
+          
         self.low_pass_filter_0 = filter.fir_filter_ccf(int(samp_rate/channel_width), firdes.low_pass(
         	1, samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((audio_gain, ))
-        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.audio_sink_0 = audio.sink(48000, "", True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
         	quad_rate=480e3,
         	audio_decimation=10,
@@ -128,14 +142,14 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.qtgui_sink_x_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.qtgui_sink_x_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "lesson1_fmradio")
@@ -147,6 +161,7 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self._samp_rate_callback(self.samp_rate)
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
@@ -169,6 +184,7 @@ class lesson1_fmradio(gr.top_block, Qt.QWidget):
 
     def set_center_freq(self, center_freq):
         self.center_freq = center_freq
+        self.qtgui_sink_x_0.set_frequency_range(self.center_freq, 2e6)
         self.osmosdr_source_0.set_center_freq(self.center_freq, 0)
         self.analog_sig_source_x_0.set_frequency(self.center_freq - self.channel_freq)
 
