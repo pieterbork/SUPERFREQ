8b66c86a80f59528d5d82297d9579de69e165f56
carlos <cala9649@colorado.edu>
2018-03-15 20:56:14 +0000

    initial zigbee without gui

---
+++ src/hackrf/SDR_Testing/zigbee_rftap_nox.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: IEEE 802.15.4 Transceiver using OQPSK PHY
+# Generated: Thu Mar 15 20:38:54 2018
+##################################################
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from epy_block_0 import blk
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from ieee802_15_4_oqpsk_phy import ieee802_15_4_oqpsk_phy  # grc-generated hier_block
+from optparse import OptionParser
+import foo
+import ieee802_15_4
+import osmosdr
+import rftap
+import time
+
+
+class zigbee_rftap_nox(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "IEEE 802.15.4 Transceiver using OQPSK PHY")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.gain = gain = 50
+        self.freq = freq = 2.45e9
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rftap_rftap_encap_0 = rftap.rftap_encap(2, 195, "")
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.osmosdr_source_0.set_sample_rate(4e6)
+        self.osmosdr_source_0.set_center_freq(freq, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(gain, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna("", 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+          
+        self.ieee802_15_4_rime_stack_0 = ieee802_15_4.rime_stack(([129]), ([131]), ([132]), ([23,42]))
+        self.ieee802_15_4_oqpsk_phy_0 = ieee802_15_4_oqpsk_phy()
+        self.ieee802_15_4_mac_0 = ieee802_15_4.mac(True)
+        self.foo_wireshark_connector_0 = foo.wireshark_connector(195, False)
+        self.epy_block_0 = blk()
+        self.blocks_socket_pdu_0 = blocks.socket_pdu("UDP_CLIENT", "127.0.0.1", "52001", 10000, False)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "/tmp/sensor.pcap", False)
+        self.blocks_file_sink_0.set_unbuffered(True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.epy_block_0, 'out'), (self.rftap_rftap_encap_0, 'in'))    
+        self.msg_connect((self.ieee802_15_4_mac_0, 'pdu out'), (self.foo_wireshark_connector_0, 'in'))    
+        self.msg_connect((self.ieee802_15_4_mac_0, 'pdu out'), (self.ieee802_15_4_oqpsk_phy_0, 'txin'))    
+        self.msg_connect((self.ieee802_15_4_mac_0, 'app out'), (self.ieee802_15_4_rime_stack_0, 'fromMAC'))    
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.epy_block_0, 'in'))    
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.foo_wireshark_connector_0, 'in'))    
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.ieee802_15_4_mac_0, 'pdu in'))    
+        self.msg_connect((self.ieee802_15_4_rime_stack_0, 'toMAC'), (self.ieee802_15_4_mac_0, 'app in'))    
+        self.msg_connect((self.rftap_rftap_encap_0, 'out'), (self.blocks_socket_pdu_0, 'pdus'))    
+        self.connect((self.foo_wireshark_connector_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.ieee802_15_4_oqpsk_phy_0, 0), (self.blocks_null_sink_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.ieee802_15_4_oqpsk_phy_0, 0))    
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.osmosdr_source_0.set_gain(self.gain, 0)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.osmosdr_source_0.set_center_freq(self.freq, 0)
+
+
+def main(top_block_cls=zigbee_rftap_nox, options=None):
+    if gr.enable_realtime_scheduling() != gr.RT_OK:
+        print "Error: failed to enable real-time scheduling."
+
+    zb_channels = [1000000 * (2400 + 5 * (i - 10)) for i in range(11, 27)]
+    print(zb_channels)
+    tb = top_block_cls()
+    tb.start()
+    for freq in zb_channels:
+        print("\nSetting frequency to {} GHz".format(freq/1e9))
+        tb.set_freq(freq)
+        time.sleep(10)
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
