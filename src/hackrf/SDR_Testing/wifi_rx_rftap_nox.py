55e6f9f3ef39726739ecccf6495a5a37c5289561
carlos <carlos.lawrence@colorado.edu>
2018-03-27 18:12:56 +0000

    moved scan functionality back in to nox.py files

--- src/hackrf/SDR_Testing/wifi_rx_rftap_nox.py
+++ src/hackrf/SDR_Testing/wifi_rx_rftap_nox.py
@@ -18,6 +18,16 @@ import ieee802_11
 import osmosdr
 import rftap
 
+import argparse,time,atexit
+from collections import OrderedDict
+
+parser = argparse.ArgumentParser(description="Run SUPERFREQ wifi scan")
+parser.add_argument("--ch", default="", help="A comma separated list of wifi channel numbers, default is all channels, channels 7,8,9,&11 must be differentiated between 2.4 and 5 GHz (e.g. 7_24 for 2.4 GHz channel 7 and 7_5 for 5 GHz channel 7), unknown channels will be ignored")
+parser.add_argument("-t", default=30, type=float, help="Total scan time in seconds, default is 30 seconds")
+args = parser.parse_args()
+
+default_wifi_freqs = OrderedDict([("1", 2.412e9), ("2", 2.417e9), ("3", 2.422e9), ("4", 2.427e9), ("5", 2.432e9), ("6", 2.437e9), ("7_24", 2.442e9), ("8_24", 2.447e9), ("9_24", 2.452e9), ("10", 2.457e9), ("11_24", 2.462e9), ("7_5", 5.035e9), ("8_5", 5.040e9), ("9_5", 5.045e9), ("11_5", 5.055e9), ("12", 5.060e9), ("16", 5.080e9), ("34", 5.170e9), ("36", 5.180e9), ("38", 5.190e9), ("40", 5.200e9), ("42", 5.210e9), ("44", 5.220e9), ("46", 5.230e9), ("48", 5.240e9), ("50", 5.250e9), ("52", 5.260e9), ("54", 5.270e9), ("56", 5.280e9), ("58", 5.290e9), ("60", 5.300e9), ("62", 5.310e9), ("64", 5.320e9), ("100", 5.500e9), ("102", 5.510e9), ("104", 5.520e9), ("106", 5.530e9), ("108", 5.540e9), ("110", 5.550e9), ("112", 5.560e9), ("114", 5.570e9), ("116", 5.580e9), ("118", 5.590e9), ("120", 5.600e9), ("122", 5.610e9), ("124", 5.620e9), ("126", 5.630e9), ("128", 5.640e9), ("132", 5.660e9), ("134", 5.670e9), ("136", 5.680e9), ("138", 5.690e9), ("140", 5.700e9), ("142", 5.710e9), ("144", 5.720e9), ("149", 5.745e9), ("151", 5.755e9), ("153", 5.765e9), ("155", 5.775e9), ("157", 5.785e9), ("159", 5.795e9), ("161", 5.805e9), ("165", 5.825e9)])
+
 
 class wifi_rx_rftap_nox(gr.top_block):
 
@@ -132,4 +142,50 @@ class wifi_rx_rftap_nox(gr.top_block):
     def set_chan_est(self, chan_est):
         self.chan_est = chan_est
         self.ieee802_11_frame_equalizer_0.set_algorithm(self.chan_est)
+
+
+def display_wifi_channel(ch):
+	ghz = ""
+	channel_num = ""
+	if "_" in ch:
+		tmp = ch.split("_")
+		if (tmp[1] == "24"):
+			channel_num = tmp[0]
+			ghz = "2.4"
+		elif (tmp[1] == "5"):
+			channel_num = tmp[0]
+			ghz = "5"
+		else:
+			return "error - {}".format(ch)
+	elif (int(ch) < 11):
+		channel_num = ch
+		ghz = "2.4"
+	elif (int(ch) > 11):
+		channel_num = ch
+		ghz = "5"
+	else:
+		return "error - {}".format(ch)
+	return "WiFi - {0} GHz Channel {1}".format(ghz, channel_num)
+
+
+if __name__ == "__main__":
+	scan_channels = OrderedDict()
+	if(args.ch == ""):
+		scan_channels = default_wifi_freqs
+	else:
+		for ch in args.ch.split(","):
+			try:
+				scan_channels[ch] = default_wifi_freqs[ch]
+			except KeyError:
+				pass
+	if(len(scan_channels) > 0):
+		wifi_tb = wifi_rx_rftap_nox()
+		wifi_tb.start()
+		for ch in scan_channels:
+			print("\n\nSetting radio to {}".format(display_wifi_channel(ch)))
+			wifi_tb.set_freq(scan_channels[ch])
+			time.sleep(float(args.t)/len(scan_channels))
+		wifi_tb.stop()
+		wifi_tb.wait()
+
 
--- src/hackrf/SDR_Testing/zigbee_rftap_nox.py
+++ src/hackrf/SDR_Testing/zigbee_rftap_nox.py
@@ -23,6 +23,63 @@ import ieee802_15_4
 import osmosdr
 import rftap
 
+from scapy.all import *
+from killerbee import * 
+from killerbee.scapy_extensions import *
+
+import argparse,time,atexit
+from collections import OrderedDict
+
+parser = argparse.ArgumentParser(description="Run SUPERFREQ zigbee scans")
+parser.add_argument("--ch", default="", help="A comma separated list of zigbee channel numbers, default is all channels, unknown channels will be ignored")
+parser.add_argument("-t", default=30, type=float, help="Total scan time in seconds, default is 30 seconds")
+args = parser.parse_args()
+
+
+default_zigbee_freqs = OrderedDict([("11", 2.405e9), ("12", 2.410e9), ("13", 2.415e9), ("14", 2.420e9), ("15", 2.425e9), ("16", 2.430e9), ("17", 2.435e9), ("18", 2.440e9), ("19", 2.445e9), ("20", 2.450e9), ("21", 2.455e9), ("22", 2.460e9), ("23", 2.465e9), ("24", 2.470e9), ("25", 2.475e9), ("26", 2.480e9)])
+
+ZB_Layers = [ \
+    Dot15d4, \
+    Dot15d4FCS, \
+    Dot15d4Beacon, \
+    Dot15d4Data, \
+    Dot15d4Ack, \
+    Dot15d4Cmd, \
+    ZigbeeNWK, \
+    ZigBeeBeacon, \
+    ZigbeeSecurityHeader, \
+    ZigbeeAppDataPayload, \
+    ZigbeeAppCommandPayload, \
+]
+ZB_Layers_Names = [ \
+    "Dot15d4", \
+    "Dot15d4FCS", \
+    "Dot15d4Beacon", \
+    "Dot15d4Data", \
+    "Dot15d4Ack", \
+    "Dot15d4Cmd", \
+    "ZigbeeNWK", \
+    "ZigBeeBeacon", \
+    "ZigbeeSecurityHeader", \
+    "ZigbeeAppDataPayload", \
+    "ZigbeeAppCommandPayload" \
+]
+
+zb_addrs = { \
+    'src_addr':'00:00:00:00:00:00', \
+    'dest_addr':'00:00:00:00:00:00', \
+    'extended_pan_id':'00:00:00:00:00:00', \
+    'src_addr':0xffff, \
+    'source':'00:00:00:00:00:00', \
+    #'source':0xffff, \
+    'src_panid':0xffff, \
+    'ext_src':'00:00:00:00:00:00', \
+    'dest_panid':0xffff, \
+    'dest_addr':0x0, \
+    'destination':0xffff \
+}
+addr_names = zb_addrs.keys()
+
 
 class zigbee_rftap_nox(gr.top_block):
 
@@ -91,3 +148,66 @@ class zigbee_rftap_nox(gr.top_block):
     def set_freq(self, freq):
         self.freq = freq
         self.osmosdr_source_0.set_center_freq(freq, 0)
+
+
+def detect_layer(pkt,layer):
+    if not pkt.haslayer(layer):
+        return False
+    return True
+
+def parse_zigbee_scan():
+	layer_count = {}
+	for l in ZB_Layers:
+		layer_count[ZB_Layers_Names[ZB_Layers.index(l)]] = 0
+	zb_file = "/tmp/sensor.pcap"
+	packets = kbrdpcap(zb_file)
+	for pkt in packets:
+		out_str_li = []
+		if detect_layer(pkt, ZigbeeNWK):
+			pkt_nwk = pkt.getlayer(ZigbeeNWK).fields
+			print(pkt_nwk)
+			out_str_li.append("src: {}".format(pkt_nwk['source']))
+			out_str_li.append("dst: {}".format(pkt_nwk['destination']))
+			if "ext_src" in pkt_nwk:
+				out_str_li.append("ext_src: {}".format(':'.join(x.encode('hex') for x in struct.pack('>Q',pkt_nwk['ext_src']))))
+			else:
+				out_str_li.append("ext_src: XX:XX:XX:XX:XX:XX:XX:XX")
+			if "ext_dst" in pkt_nwk:
+				out_str_li.append("ext_dst: {}".format(':'.join(x.encode('hex') for x in struct.pack('>Q',pkt_nwk['ext_dst']))))
+			else:
+				out_str_li.append("ext_dst: XX:XX:XX:XX:XX:XX:XX:XX")
+
+		if detect_layer(pkt, ZigbeeSecurityHeader):
+			pkt_nwk = pkt.getlayer(ZigbeeSecurityHeader).fields
+			if "source" in pkt_nwk:
+				out_str_li.append("sec_src: {}".format(':'.join(x.encode('hex') for x in struct.pack('>Q',pkt_nwk['source']))))
+			else:
+				out_str_li.append("sec_src: XX:XX:XX:XX:XX:XX:XX:XX")
+			if "destination" in pkt_nwk:
+				out_str_li.append("sec_dst: {}".format(':'.join(x.encode('hex') for x in struct.pack('>Q',pkt_nwk['destination']))))
+			else:
+				out_str_li.append("sec_dst: XX:XX:XX:XX:XX:XX:XX:XX")
+		print(out_str_li)
+
+
+if __name__ == "__main__":
+	scan_channels = OrderedDict()
+	if (args.zch == ""):
+		scan_channels = default_zigbee_freqs
+	else:
+		for ch in args.zch.split(","):
+			try:
+				scan_channels[ch] = default_zigbee_freqs[ch]
+			except:
+				pass
+	if(len(scan_channels) > 0):
+		zb_tb = zigbee_rftap_nox()
+		zb_tb.start()
+		for ch in scan_channels:
+			print("\nSetting radio to Zigbee - Ch {}".format(ch))
+			zb_tb.set_freq(scan_channels[ch])
+			time.sleep(float(args.t)/len(scan_channels))
+		zb_tb.stop()
+		zb_tb.wait()
+		parse_zigbee_scan()
+
