41c2aee466b1464640cd5d9ab027ec45903eb3eb
AncNomKad <kadcooper@gmail.com>
2018-03-28 16:32:01 -0600

    Reformatted graphical output for UnitTest (to push to main app). Added new functionality for wifi_csv_filter in order to get good graph output.

--- src/UnitTests/Infrastructure_UnitTests/csv_operations/generate_graph.py
+++ src/UnitTests/Infrastructure_UnitTests/csv_operations/generate_graph.py
@@ -3,7 +3,7 @@
 #author : Kade Cooper kaco0964@colorado.edu
 #name : generate_graph.py
 #purpose : Test numpy and matplotlib libraries for Python2
-#date : 2018.03.18
+#date : 2018.03.28
 #version: 1.0.0
 #version notes (latest): Compatible w/ python2
 
@@ -16,8 +16,11 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 #Create lists
-labels=[]
-perc=[]
+#labels=[]
+#perc=[]
+
+xaxis=[]
+yaxis=[]
 
 #Format of csv: Duration,Frame Control,Subtype,SSID,seq nr,mac 1, mac occurence
 #Color scheme
@@ -25,31 +28,52 @@ a = np.random.random(40)
 cs = cm.Set1(np.arange(40)/40.)
 
 sample_csv = cp_rm.src_file
-print sample_csv
 
+"""
 #Check csv file
 if not os.path.isfile(sample_csv):
     print("\n The MCP has derezzed the file!\n")
     sys.exit()
 else:
+    counter = 0
     with open(sample_csv) as csvFile:
         #Use csv parser
         reader = csv.reader(csvFile, delimiter=',')
         for row in reader:
-            labels.append(row[3]+"-"+row[6])
-            perc.append(float(row[7]))
+            if counter < 10:
+                labels.append(row[3]+"-"+row[8])
+                perc.append(float(row[9]))
+            counter += 1
             
     #Add data to plot
     plt.pie(perc, labels=labels, autopct='%1.1f%%', colors=cs, shadow=True, startangle=90)
 
-    #Use for changing font colors
-    """   _, _, autotexts = plt.pie(perc, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-    for autotext in autotexts:
-        autotext.set_color('grey')
-    """
+"""
+#Check csv file
+if not os.path.isfile(sample_csv):
+    print("\n The MCP has derezzed the file!\n")
+    sys.exit()
+else:
+    ssid_list = list(range(0,20))
+    with open(sample_csv) as csvFile:
+        #Use csv parser
+        reader = csv.reader(csvFile, delimiter=',')
+        next(reader, None) #Skip headers
+        for row in reader:
+            #Check for blank lines
+            xaxis.append(row[3]+"-"+row[8].strip())
+            yaxis.append(int(row[9]))
 
+    #Add values to chart
+    barWidth = 1.0
+    plt.bar(ssid_list, yaxis, barWidth, color=cs)
+    plt.xticks(ssid_list, xaxis, rotation=(45), fontsize = 10, va='bottom', ha='left')
+    
+    #Labels
+    plt.ylabel("Occurrence(s)")
+    plt.xlabel("SSID - Frequency")
     #Rounds plot
-    plt.axis('equal')
+    #plt.axis('tight')
     plt.title("Frequency of MAC Addresses\n")
 
     #Save Image
--- src/UnitTests/Infrastructure_UnitTests/csv_operations/wifi_csv_filter.bash
+++ src/UnitTests/Infrastructure_UnitTests/csv_operations/wifi_csv_filter.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #author : Kade Cooper kaco0964@colorado.edu
-#name : csv_filter.bash
+#name : wifi_csv_filter.bash
 #purpose : Filter csv files to help generate graphs from the terminal application
 #date : 2018.03.28
-#version: 1.0.10
+#version: 1.1.01
 
 
 #Detect the latest file to be added
@@ -23,7 +23,7 @@ sed "s/^[[:space:]]*//" -i unique_lines.txt
 sed 's/, \+\| \+,/,/g' -i unique_lines.txt
 
 #Remove all matched strings from our wordfile.txt. VERIFIED!
-sed -e "$(sed 's:.*:s/&//ig:' wordfile.txt)" unique_lines.txt > cleaned_unique_lines.txt
+sed -e "$(sed 's:.*:s/&//ig:' wifi_wordfile.txt)" unique_lines.txt > cleaned_unique_lines.txt
 
 #Cut all lines but the first column. VERIFIED!
 awk  '{$1=$1; print $1}' cleaned_unique_lines.txt > occurences.txt
@@ -34,17 +34,30 @@ sed -i -r 's/\S+(\s+)?//1' cleaned_unique_lines.txt
 #Find total number of lines. VERIFIED!
 TOTALLINES=$(wc -l tmp_unfiltered.csv | awk '{print $1}')
 
+
 #Calculate percentages for each
 while read i; do
   printf '%.4f\n' "$(echo "scale=4;$i/$TOTALLINES" | bc)" >> percentage.txt
 done <occurences.txt
 
+
 #Attach percentages to the end of each row in the unique_lines.txt
-paste -d"," cleaned_unique_lines.txt percentage.txt > filtered.csv
+#paste -d"," cleaned_unique_lines.txt percentage.txt > wifi_filtered.csv
+
+#For whole number use below
+paste -d"," cleaned_unique_lines.txt occurences.txt > wifi_filtered.csv
+
+#Remove any lines which do not have the correct number of columns
+awk -F',' 'NF==10' wifi_filtered.csv  > checked_wifi_filtered.csv
 
 
 #After all is said and done, use Vim in Ex mode to add header column to the file for importing into the database
-ex -sc '1i|duration,frame,subtype,ssid,seq_nr,mac_address_1,mac_address_2,mac_address_3,frequency,percentage' -cx filtered.csv
+#ex -sc '1i|duration,frame,subtype,ssid,seq_nr,mac_address_1,mac_address_2,mac_address_3,frequency,percentage' -cx checked_wifi_filtered.csv
+ex -sc '1i|duration,frame,subtype,ssid,seq_nr,mac_address_1,mac_address_2,mac_address_3,frequency,occurrence' -cx checked_wifi_filtered.csv
+
+
+#Finally, in order for our graphs to have any meaning, display the top ten occurrences
+sed -n -e '1,21p' checked_wifi_filtered.csv > wifi_data_graph_ready.csv
 
 #Remove files this script made
-rm tmp_unfiltered.csv occurences.txt unique_lines.txt cleaned_unique_lines.txt percentage.txt 
+rm tmp_unfiltered.csv occurences.txt unique_lines.txt cleaned_unique_lines.txt percentage.txt wifi_filtered.csv checked_wifi_filtered.csv
