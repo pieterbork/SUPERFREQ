cae6abfa7de41e7f65d8885efc9743132209656e
pieterbork <pieter.bork@colorado.edu>
2018-03-30 22:47:06 +0000

    added job_id yeet

--- src/hackrf/SDR_Testing/read_scan_data.py
+++ src/hackrf/SDR_Testing/read_scan_data.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sdr_modules import system_module_operations
 
 bt_out = "/tmp/bt_out.txt"
 xbee_out = "/tmp/zb_out.txt"
@@ -9,18 +8,17 @@ bt_clean = "bt_clean.txt"
 xbee_clean = "xbee_clean.txt"
 wifi_clean = "wifi_clean.txt"
 
-db_path = system_module_operations.getSourceDir()
-db_path += 'src/infrastructure/database/SUPERFREQ.db'
+db_path = '../../infrastructure/database/SUPERFREQ.db'
 
 #Reads wifi_out file, parses records, returns list
-def parse_wifi_records():
+def parse_wifi_records(job_id):
 	records = set()
 	#Indexes in each record for ssid, mac1, mac2, mac3, freq
 	items = [3,5,6,7,8]
 	with open(wifi_out) as fh:
 		lines = fh.readlines()
 		for line in lines:
-			record = []
+			record = [job_id]
 			parts = line.split(",")
 			if len(parts) != 9:
 				continue
@@ -32,7 +30,7 @@ def parse_wifi_records():
 	return records
 
 #Reads bt_out file, parses records, returns list
-def parse_bt_records():
+def parse_bt_records(job_id):
 	records = set()
 	with open(bt_out, 'r') as fh:
 		lines = fh.readlines()
@@ -43,11 +41,11 @@ def parse_bt_records():
 				continue
 			channel = parts[2].replace("Ch", "")
 			mac = parts[8].replace("AdvA:", "")
-			records.add((channel, mac))
+			records.add((job_id, channel, mac))
 	return list(records)
 
 #Reads zb_out file, parses records, returns list
-def parse_zb_records():
+def parse_zb_records(job_id):
 	records = set()
 	with open(xbee_out, 'r') as fh:
 		lines = fh.readlines()
@@ -56,7 +54,7 @@ def parse_zb_records():
 			parts = line.split()
 			if len(parts) != 12:
 				continue
-			record = (parts[1], parts[3], parts[5], parts[7], parts[9], parts[11])
+			record = (job_id, parts[1], parts[3], parts[5], parts[7], parts[9], parts[11])
 			records.add(record)
 	return list(records)
 
@@ -64,34 +62,81 @@ def parse_zb_records():
 def insert_wifi_records(records):
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.executemany("INSERT INTO Wifi (ssid, mac1, mac2, mac3, freq) VALUES (?, ?, ?, ?, ?)", records)
+		cursor.executemany("INSERT INTO Wifi (job_id, ssid, mac1, mac2, mac3, freq) VALUES (?, ?, ?, ?, ?, ?)", records)
 
 #Insert list of bluetooth records with sqlite
 def insert_bt_records(records):
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.executemany("INSERT INTO Bluetooth (channel, mac) VALUES (?, ?)", records)
+		cursor.executemany("INSERT INTO Bluetooth (job_id, channel, mac) VALUES (?, ?, ?)", records)
 
 #Insert list of zigbee records with sqlite
 def insert_zb_records(records):
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.executemany("INSERT INTO Zigbee (src, dst, ext_src, ext_dst, sec_src, sec_dst) VALUES (?, ?, ?, ?, ?, ?)", records)
+		cursor.executemany("INSERT INTO Zigbee (job_id, src, dst, ext_src, ext_dst, sec_src, sec_dst) VALUES (?, ?, ?, ?, ?, ?, ?)", records)
+
+def insert_job(name):
+	if not get_job_id(name):
+		with sqlite3.connect(db_path) as conn:
+			cursor = conn.cursor()
+			cursor.execute("INSERT INTO Jobs (name) VALUES (?)", (name,))
 
 def create_wifi_table():
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS Wifi(ssid VARCHAR(120), mac1 VARCHAR(120), mac2 VARCHAR(120), mac3 VARCHAR(120), freq VARCHAR(10))")
+		cursor.execute("CREATE TABLE IF NOT EXISTS\
+			Wifi(\
+				job_id INTEGER,\
+				ssid VARCHAR(120),\
+				mac1 VARCHAR(120),\
+				mac2 VARCHAR(120),\
+				mac3 VARCHAR(120),\
+				freq VARCHAR(10),\
+				FOREIGN KEY(job_id) REFERENCES Jobs(id))")
 
 def create_bt_table():
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS Bluetooth(channel INT(2), mac VARCHAR(120))")
+		cursor.execute("CREATE TABLE IF NOT EXISTS\
+			Bluetooth(\
+				job_id integer,\
+				channel INT(2),\
+				mac VARCHAR(120),\
+				FOREIGN KEY(job_id) REFERENCES Jobs(id))")
 
 def create_zb_table():
 	with sqlite3.connect(db_path) as conn:
 		cursor = conn.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS Zigbee(src INT(8), dst INT(8), ext_src VARCHAR(120), ext_dst VARCHAR(120), sec_src VARCHAR(120), sec_dst VARCHAR(120))")
+		cursor.execute("CREATE TABLE IF NOT EXISTS\
+			Zigbee(\
+				job_id INTEGER,\
+				src INT(8),\
+				dst INT(8),\
+				ext_src VARCHAR(120),\
+				ext_dst VARCHAR(120),\
+				sec_src VARCHAR(120),\
+				sec_dst VARCHAR(120),\
+				FOREIGN KEY(job_id) REFERENCES Jobs(id))")
+
+def create_job_table():
+	with sqlite3.connect(db_path) as conn:
+		cursor = conn.cursor()
+		cursor.execute("CREATE TABLE IF NOT EXISTS\
+				Jobs(\
+					id integer primary key,\
+					name VARCHAR(120))")
+
+def get_job_id(name):
+	job_id = None
+	with sqlite3.connect(db_path) as conn:
+		cursor = conn.cursor()
+		sql = "SELECT * FROM Jobs WHERE name='{}'".format(name)
+		sql_records = cursor.execute(sql)
+	records = [record for record in sql_records]
+	if len(records) > 0:
+		job_id = records[0][0]
+	return job_id
 
 #Reads records from table
 def get_records_from_table(table):
@@ -122,20 +167,26 @@ def delete_table(table):
 		sql = "DROP TABLE {}".format(table)
 		cursor.execute(sql)
 
+create_job_table()
+insert_job("CARLOS_SUCKS")
+job_id = get_job_id("CARLOS_SUCKS")
+print(job_id)
+
 ##Wifi##
 create_wifi_table()
 clear_table("Wifi")
-records = parse_wifi_records()
+records = parse_wifi_records(job_id)
+
 insert_wifi_records(records)
 records = get_records_from_table("Wifi")
 for record in records:
 	print(record)
 
-
 ##Bluetooth
 create_bt_table()
 clear_table("Bluetooth")
-records = parse_bt_records()
+records = parse_bt_records(job_id)
+
 insert_bt_records(records)
 records = get_records_from_table("Bluetooth")
 for record in records:
@@ -145,7 +196,7 @@ for record in records:
 ##Zigbee##
 create_zb_table()
 clear_table("Zigbee")
-records = parse_zb_records()
+records = parse_zb_records(job_id)
 insert_zb_records(records)
 records = get_records_from_table("Zigbee")
 for record in records:
