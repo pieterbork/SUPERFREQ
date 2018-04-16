2320be686e0ae52da1e63b1274ca7e20e3f09a6d
Carlos <carlos.lawrence@colorado.edu>
2018-04-16 10:26:57 -0600

    Added GPL lisence headers

--- src/hackrf/SDR_Testing/zigbee_rftap_nox.py
+++ src/hackrf/SDR_Testing/zigbee_rftap_nox.py
@@ -4,6 +4,23 @@
 # GNU Radio Python Flow Graph
 # Title: IEEE 802.15.4 Transceiver using OQPSK PHY
 # Generated: Thu Mar 15 20:38:54 2018
+#
+# Copyright (C) 2018 Team SUPERFREQ, CU Boulder ITP <itp@colorado.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 ##################################################
 
 import os
